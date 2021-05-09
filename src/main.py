5e9cbe5d6664bd16fcff0304e811de19c1d9dce9
Lorenzo Tomasetti <lori.tomasetti@hotmail.it>
2021-05-09 11:38:33 +0200

    prima parrte funzione

---
+++ src/main.py
@@ -0,0 +1,39 @@
+import json
+import random
+
+OP_CONCAT=' '
+OP_ALT='|'
+OP_REP='*'
+NULL_SMIB='ε'
+
+
+
+def espressione_regolare(dict):
+    #ricerca elemnto I dentro un array di tutti i nodi
+    for el in dict:
+        if el['type']=="I":
+            if len(el['type'])>0:
+                #da gestire con gli oggetti
+                with open('data/stateN0.json') as f:
+                  n0 = json.load(f)
+                n0['outgoings'][0]['node']=el['name']
+                dict.append(n0)
+            else:
+                n0=el
+                n0['name']="N0"
+            break
+        print(1)
+
+
+
+
+
+
+
+
+
+
+with open('data/graph.json') as f:
+  data = json.load(f)
+#testare con random
+espressione_regolare(data)
